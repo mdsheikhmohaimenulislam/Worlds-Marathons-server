0a131913b3abc347c36332356e6e78a08c2156e7
Mohai menul islam <mdsheikhmohaimenulislam@gmail.com>
2025-08-10 19:25:05 +0600

    deleted section done

--- index.js
+++ index.js
@@ -296,12 +296,12 @@ app.get("/user", async (req, res) => {
     });
 
     // Deleted section
-    // app.delete("/users/:id", async (req, res) => {
-    //   const id = req.params.id;
-    //   const query = { _id: new ObjectId(id) };
-    //   const result = usersCollection.deleteOne(query);
-    //   res.send(result);
-    // });
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = usersCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // await client.db("admin").command({ ping: -1 });
     // console.log(
